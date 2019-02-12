15d63885f5eee7fc6da7ba0d560a7b125fd69011
Ming Chai <ming.chai4@gmail.com>
2019-02-12 11:27:51 -0800

    set up authentication in application controller

--- auction_api/app/controllers/application_controller.rb
+++ auction_api/app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  private
+
+    def current_user
+        if session[:user_id].present?
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    def user_signed_in?
+        current_user.present?
+    end
+    
+    def authenticate_user!
+        unless user_signed_in?
+            flash[:danger] = "Please sign in or sign up"
+            redirect_to new_session_path
+        end
+    end
+    
 end
