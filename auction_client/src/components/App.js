99c521b6408e0ef1ea1643a32644c93482d803d7
Ming Chai <ming.chai4@gmail.com>
2019-02-12 12:18:29 -0800

    create basic setup for React application

---
+++ auction_client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+import WelcomePage from './WelcomPage';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <WelcomePage/>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ auction_client/src/components/WelcomPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WelcomePage = () =>{
+  return(
+    <main>
+      <h1>Welcome Page</h1>
+    </main>
+  )
+}
+
+export default WelcomePage;
