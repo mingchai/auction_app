38661ad1dfa5be2ad263f413b2e73429176edca3
Ming Chai <ming.chai4@gmail.com>
2019-02-12 10:52:10 -0800

    update routes to include users and bids

--- auction_api/config/routes.rb
+++ auction_api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :auctions, only [:create, :show, :index]
+  resources :users, only: [:create, :show, :destroy]do
+    resources :auctions, only: [:create, :show, :index] do
+      resources :bids, only: [:create, :destroy]
+      end
+  end
 end
