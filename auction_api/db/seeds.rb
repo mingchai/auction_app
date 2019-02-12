1f3ce2b06339e99e15b416852fc301c241069c1f
Ming Chai <ming.chai4@gmail.com>
2019-02-12 10:03:05 -0800

    create inital seed file to test auction api routes

--- auction_api/db/seeds.rb
+++ auction_api/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Auction.destroy_all
+
+ActiveRecord::Base.connection.reset_pk_sequence!('auctions')
+
+25.times do
+  Auction.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::IDNumber.valid,
+    price: Faker::Commerce.price(range = 10.0..10000.0, as_string = false),
+    reserve: ["true", "false"].sample,
+    expiry_date: Faker::Date.forward(60)
+  )
+end
+auctions = Auction.all
+
+p "Created #{auctions.count} auction entries"
