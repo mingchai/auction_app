67c031e86372fc545a78f101fd71971e112501b7
Ming Chai <ming.chai4@gmail.com>
2019-02-12 11:50:49 -0800

    adjust routes to find current user id

--- auction_api/config/routes.rb
+++ auction_api/config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :session, only: [:create, :destroy]
   
+  resources :users, only:[] do
+    get :current, on: :collection
+  end
+  
   resources :users, only: [:create, :show, :destroy]do
     resources :auctions, only: [:create, :show, :index] do
       resources :bids, only: [:create, :destroy]
       end
   end
   
-  resources :users, only:[] do
-    get :current, on: :collection
-  end
 end
