84241ba7f0f811e6ea8a0b67f964281f4daf0341
Ming Chai <ming.chai4@gmail.com>
2019-02-12 11:40:07 -0800

    auction actions created and tested in Postman

--- auction_api/app/controllers/auctions_controller.rb
+++ auction_api/app/controllers/auctions_controller.rb
@@ -1,12 +1,26 @@
 class AuctionsController < ApplicationController
   def create
-    
+    auction = Auction.new auction_params
+    auction.user = current_user
+    if auction.save!
+      render json:{id: auction.id}
+    else
+      render json: {errors: auction.errors}, status: 422
+    end
   end
+
   def show
-    
+    auction = Auction.find params[:id]
+    render json: auction
   end
+
   def index
     auctions = Auction.all.order(created_at: :desc)
     render json: auctions
+  end
+
+  private
+  def auction_params
+    params.require(:auction).permit(:title, :description, :price, :reserve, :reserve_price, :expiry_date)
   end
 end
