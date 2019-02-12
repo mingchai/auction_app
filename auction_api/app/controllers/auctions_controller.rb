a514183a1e6a30a77272eaf2e09a0529aeec06f6
Ming Chai <ming.chai4@gmail.com>
2019-02-12 10:09:42 -0800

    add create, show and index actions to auctions controller

--- auction_api/app/controllers/auctions_controller.rb
+++ auction_api/app/controllers/auctions_controller.rb
@@ -1,2 +1,12 @@
 class AuctionsController < ApplicationController
+  def create
+    
+  end
+  def show
+    
+  end
+  def index
+    auctions = Auction.all.order(created_at: :desc)
+    render json: auctions
+  end
 end
