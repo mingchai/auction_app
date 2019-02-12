34c6e58b9f7c97495094e47b1e3da76f36d098a0
Ming Chai <ming.chai4@gmail.com>
2019-02-12 10:10:33 -0800

    rework routes for auctions

--- auction_api/config/routes.rb
+++ auction_api/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json} do
-    namespace :v1 do
-      resources :auctions, only: [:create, :show, :index]
-    end
-  end
-  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :auctions, only [:create, :show, :index]
 end
